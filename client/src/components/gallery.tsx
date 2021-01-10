53a1a1042b70d14671ab27f6a1f22c74e31ff9d8
llam <llam@g.hmc.edu>
2021-01-10 16:14:23 -0500

    implement infinite scroll for home page

--- client/src/components/gallery.tsx
+++ client/src/components/gallery.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-// import UnsplashImage from './unsplash_image'
 import axios from 'axios';
 import Container from '@material-ui/core/Container'
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import Search from './search'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Gallery: React.FC = () => {
     const [images, setImages] = useState<any>([]);
@@ -13,30 +14,32 @@ const Gallery: React.FC = () => {
     const [search, setSearch] = useState<String>('');
     
     const unsplashAPI = "https://api.unsplash.com"
+    // const accessKey = process.env.UNSPLASH_ACCESS_KEY
 
     useEffect(() => {
+        fetchImages();
+    }, [])
+    
+    const fetchImages = () => {
         setLoading(true)
-        // const accessKey = process.env.UNSPLASH_ACCESS_KEY
         axios
             .get(`${unsplashAPI}/photos/random?client_id=DvjCg2G2B7CpZqGGEO0BJbxr6YpaOeuFt09A32zLnEY&count=10 `)
             .then((res:any) => setImages([...images, ...res.data]))
             .then((res:any) => console.log(res))
         console.log(images)
         setLoading(false)
-    }, [])
-
-    if(loading) return <CircularProgress size={100}/>
-
+    }
+    
     const getImages = () => {
         return (
             <GridList cellHeight={250} cols={3} >
                 {images.map((image:any) => {
                     return <GridListTile key={image.id}
-                                style={{ flexGrow: 1 }}
-                                cols = {(image.width/5000)}>
+                    style={{ flexGrow: 1 }}
+                    cols = {(image.width/5000)}>
                         <img 
                             srcSet={`${image.urls.regular}?w=161&fit=crop&auto=format 1x, 
-                                ${image.urls.thumb}?w=161&fit=crop&auto=format&dpr=2 2x`}
+                            ${image.urls.thumb}?w=161&fit=crop&auto=format&dpr=2 2x`}
                             alt={image.description || image.alt_description}
                         />
                     </GridListTile>
@@ -44,27 +47,36 @@ const Gallery: React.FC = () => {
             </GridList>
         )
     }
-
+    
     const handleSearchInput = (e:any) => {
         setSearch(e.target.value);
     }
-
+    
     const handleSubmit = async (e:any) => {
         e.preventDefault();
-        setLoading(true)
-        const res = await axios
-            .get(`${unsplashAPI}/search/photos?page=1&query=${search}&client_id=DvjCg2G2B7CpZqGGEO0BJbxr6YpaOeuFt09A32zLnEY&per_page=20 `)
-        console.log(res)
-        const searchRes = res.data.results
-        setImages(searchRes)
-        setLoading(false)
+        if(search !== ''){
+            setLoading(true)
+            const res = await axios
+                .get(`${unsplashAPI}/search/photos?page=1&query=${search}&client_id=DvjCg2G2B7CpZqGGEO0BJbxr6YpaOeuFt09A32zLnEY&per_page=20 `)
+            const searchRes = res.data.results
+            setImages(searchRes)
+            setLoading(false)
+        }
     }
 
+    if(loading) return <CircularProgress size={100}/>
+    
     return (
-        <Container maxWidth="lg">
-            <Search handleSearch={handleSearchInput} handleSubmit={handleSubmit}/>
-            {getImages()}
-        </Container>
+        <InfiniteScroll 
+            dataLength = {images.length}
+            next = {fetchImages}
+            hasMore = {true}
+            loader = {<LinearProgress />} >
+            <Container maxWidth="lg">
+                <Search handleSearch={handleSearchInput} handleSubmit={handleSubmit}/>
+                {getImages()}
+            </Container>
+        </InfiniteScroll>
     )
 }
 
