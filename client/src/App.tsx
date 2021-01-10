91cc3c99a842ff63484d50655e8050810a4d2ff4
llam <llam@g.hmc.edu>
2021-01-09 19:16:29 -0500

    add login register and home routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,34 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './components/home';
+import Login from './components/login';
+import Register from './components/register';
 
 function App() {
-  const [data, setData] = React.useState<any|null>(null);
+  // const [data, setData] = React.useState<any|null>(null);
 
-  const getData = () => {
-    fetch('/api')
-      .then((result) => result.text())
-      .then((res) => setData(res));
-  };
+  // const getData = () => {
+  //   fetch('/api')
+  //     .then((result) => result.text())
+  //     .then((res) => setData(res));
+  // };
   return (
-    <div className="App">
+    <Fragment>
       <header className="App-header">
         <Button>HELLO WORLD</Button>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={getData}>Click Me For Data</button>
-        {data}
       </header>
-    </div>
+
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/home" component={Home}/>
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/login.tsx
@@ -0,0 +1,11 @@
+import {Fragment} from 'react';
+
+function Login() {
+    return (
+        <Fragment>
+            <h1>Login</h1>
+        </Fragment>
+    )
+}
+
+export default Login
